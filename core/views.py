1178336016800d7b99a10c6dd7085af7af3497b2
samircd4 <61603868+samircd4@users.noreply.github.com>
2022-12-08 23:32:36 +0600

    Update views.py

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from django.db.models import Q
 # Create your views here.
 def index(request):
     if request.method == 'GET':
-        search = request.GET.get('search')
+        search = request.GET.get('search', '')
         res = Q(Q(title__icontains=search) | Q(s_link__icontains=search) | Q(desc__icontains=search))
         results = Directory.objects.filter(res)
         contex = {
